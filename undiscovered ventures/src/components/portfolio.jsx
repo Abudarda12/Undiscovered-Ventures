9b2d71dac709fc26bb8ace717ee59104af6a0277
abudarda12 <abudardaansari66@gmail.com>
2025-08-12 20:55:26 +0530

    added portfolio

--- undiscovered ventures/src/components/portfolio.jsx
+++ undiscovered ventures/src/components/portfolio.jsx
@@ -4,9 +4,19 @@ import "../style/portfolio.css";
 export default function Portfolio() {
   return (
     <div className="portfolio">
-      
+      <div className="portfolio-heading">
+        <div className="project">
+            <div className="left"></div> Projects <div className="right"></div>
+        </div>
+        <h1>PORTFOLIO</h1>
+      </div>
       <div className="portfolio-list">
-        {/* Portfolio items will be added here */}
+        <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
+         <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
+          <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
+           <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
+            <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
+             <img src="/images/5d087b9f19889c2ec7f832d64dab895a2155b5bd.jpg" alt="" height={"300px"} width={"300px"}/>
       </div>
     </div>
   );
