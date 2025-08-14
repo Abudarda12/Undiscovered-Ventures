47158bef5bfebca105ffe20805f33c01ef25b7d3
abudarda12 <abudardaansari66@gmail.com>
2025-08-15 00:11:44 +0530

    added slider

--- undiscovered ventures/src/components/client.jsx
+++ undiscovered ventures/src/components/client.jsx
@@ -1,62 +1,57 @@
-import React from 'react';
+import React from "react";
 import "../style/client.css";
 
 export default function Client() {
   return (
     <div className="client">
-      <div className="client-heading">
-        <h1>Selected Clients</h1>
-        <div className="client-subheading">
-            <p>Helping <i>brands</i><br />to standout</p>
+      <div className="client-content">
+        <div className="client-heading">
+          <h1>Selected Clients</h1>
+          <div className="client-subheading">
+            <p>
+              Helping <i>brands</i>
+              <br />
+              to standout
+            </p>
+          </div>
         </div>
+        <div className="client-list">
+          
+            <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
+         
+            <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
+          
+     
+            <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
+     
+            <img src="/images/44.png" alt="" height={"80px"} width={"100px"} />
+     
+            <img src="/images/55.png" alt="" height={"80px"} width={"100px"} />
+        
+            <img src="/images/66.png" alt="" height={"80px"} width={"100px"} />
+  
+            <img src="/images/77.png" alt="" height={"80px"} width={"100px"} />
+     
+            <img src="/images/88.png" alt="" height={"80px"} width={"100px"} />
+          
+            <img src="/images/99.png" alt="" height={"80px"} width={"100px"} />
+          
+            <img src="/images/10.jpg" alt="" height={"80px"} width={"100px"} />
+          
+            <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
+          
+          
+            <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
+         
+         
+            <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
+        
+          
+        </div>
+        <button>
+          <a href="#">Get in Touch</a>
+        </button>
       </div>
-      <div className="client-list">
-        <div className="client-item">
-            <img src="/images/11.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/22.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/33.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/44.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/55.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/66.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/77.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/88.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/99.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/10.jpg" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/01.png" alt="" height={"80px"} width={"100px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/03.png" alt="" height={"100px"} width={"150px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/04.png" alt="" height={"100px"} width={"150px"}/>
-        </div>
-        <div className="client-item">
-            <img src="/images/05.png" alt="" height={"100px"} width={"150px"}/>
-        </div>
-      </div>
-      <button>
-        <a href="#">Get in Touch</a>
-      </button>
     </div>
   );
 }
