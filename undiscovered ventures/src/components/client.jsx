2ee9654634e56938c095c3433383dcb5541a860d
rishiraj526 <rishiraj00346@gmail.com>
2025-08-16 15:53:39 +0530

    added client

--- undiscovered ventures/src/components/client.jsx
+++ undiscovered ventures/src/components/client.jsx
@@ -4,6 +4,7 @@ import "../style/client.css";
 export default function Client() {
   return (
     <div className="client">
+     
       <div className="client-content">
         <div className="client-heading">
           <h1>Selected Clients</h1>
@@ -16,37 +17,84 @@ export default function Client() {
           </div>
         </div>
         <div className="client-list">
-          
-            <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
-         
-            <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
-          
-     
-            <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
-     
-            <img src="/images/44.png" alt="" height={"80px"} width={"100px"} />
-     
-            <img src="/images/55.png" alt="" height={"80px"} width={"100px"} />
-        
-            <img src="/images/66.png" alt="" height={"80px"} width={"100px"} />
-  
-            <img src="/images/77.png" alt="" height={"80px"} width={"100px"} />
-     
-            <img src="/images/88.png" alt="" height={"80px"} width={"100px"} />
-          
-            <img src="/images/99.png" alt="" height={"80px"} width={"100px"} />
-          
-            <img src="/images/10.jpg" alt="" height={"80px"} width={"100px"} />
-          
-            <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
-          
-          
-            <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
-         
-         
-            <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
-        
-          
+          <div className="upper-client">
+            <div className="client-cover">
+              <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+            <div className="client-cover">
+              <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+            <div className="client-cover">
+              <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/44.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/55.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/66.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/77.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/88.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/99.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/10.jpg" alt="" height={"80px"} width={"100px"} />
+            </div>
+          </div>
+          <div className="lower-client">
+            <div className="client-cover">
+              <img src="/images/11.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+            <div className="client-cover">
+              <img src="/images/22.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+            <div className="client-cover">
+              <img src="/images/33.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/44.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/55.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/66.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/77.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/88.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/99.png" alt="" height={"80px"} width={"100px"} />
+            </div>
+
+            <div className="client-cover">
+              <img src="/images/10.jpg" alt="" height={"80px"} width={"100px"} />
+            </div>
+          </div>
         </div>
         <button>
           <a href="#">Get in Touch</a>
